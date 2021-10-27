5ec84ca356244178116a8d2b1568a265cdc29c42
Reina-hub <reina@leakmited.com>
2021-10-27 10:13:51 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { DataStore } from '@aws-amplify/datastore';
+import { Leak } from './models';
+import { useEffect, useState } from 'react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
 function App() {
+
+  const [leaks, setLeaks] = useState([])
+
+  useEffect(() => {
+    const func = async () => {
+      const models = await DataStore.query(Leak)
+      setLeaks(models)
+    }
+    func()
+
+  }, [])
+
+  const createLeak = async () => {
+    const leak = {
+      lon: window.prompt('leak lon'),
+      lat: window.prompt('leak lat')
+    }
+
+    const newLeak = await DataStore.save(
+      new Leak(leak)
+    )
+    console.log(newLeak)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={createLeak}>Create Leak</button>
+      {leaks.map(leak => <div key={leak.id}>
+        <h1>{leak.lon}</h1>
+        <h1>{leak.lat}</h1>
+      </div>)}
+  </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/models/schema.js
@@ -0,0 +1,73 @@
+export const schema = {
+    "models": {
+        "Leak": {
+            "name": "Leak",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "lon": {
+                    "name": "lon",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "lat": {
+                    "name": "lat",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Leaks",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "3673b21e79cbe40da41e257ae1c9eb9e"
+};
